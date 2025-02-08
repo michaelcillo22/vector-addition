22d7042219db0fc439bfddbdcbc008d7ff57d8ce
Michael Cillo <mjcillo22@gmail.com>
2025-02-07 21:32:27 -0500

    these worked!

---
+++ src/test.cu
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+__global__ void helloCUDA() {
+    printf("Hello from CUDA!\n");
+}
+
+int main() {
+    helloCUDA<<<1, 1>>>();
+    cudaDeviceSynchronize();
+    return 0;
+}
